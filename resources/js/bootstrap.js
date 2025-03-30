bf8f5c6d0e546fa01a29009c5e8825ddc76ad442
TungTvPh81102 <quaixe121811@gmail.com>
2025-03-31 03:43:45 +0700

    add database course-mely

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,14 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true
+    // broadcaster: 'pusher',
+    // key: import.meta.env.VITE_PUSHER_APP_KEY,
+    // cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+    // wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    // wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    // wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    // forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    // enabledTransports: ['ws', 'wss'],
 });
 
 // import Echo from "laravel-echo";
